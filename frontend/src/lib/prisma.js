32959bddf3c3319788205b8ff2222bca43fadbc3
LaibaAimen7777 <laibaimen7777@gmail.com>
2026-04-13 02:19:43 +0500

    4/13_deploymet_fix

--- frontend/src/lib/prisma.js
+++ frontend/src/lib/prisma.js
@@ -5,7 +5,7 @@ const globalForPrisma = globalThis;
 
 function createPrismaClient() {
   const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL ?? "",
   });
   return new PrismaClient({ adapter });
 }
